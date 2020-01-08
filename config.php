66826532e921965cb66c087e5b5e80e7469f61b2
kaygb <kaygbtop@163.com>
2020-01-08 20:42:15 +0800

    Update config.php
    添加自定义域名选项

--- config.php
+++ config.php
@@ -1,7 +1,18 @@
 <?php
-//获取当前域名
-define("TOOLS_URL",$_SERVER['SERVER_NAME']);
-// define("TOOLS_URL","tools.kaygb.top");
+// 配置
+$config_ktools_password = "123123"; //状态监控访问密码
+$config_customurl = false;    //是否开启自定义域名，true/false
+$config_ktools_custom_url =  "";    //自定义域名，上方配置填 true 之后填入此项
 
-// 状态监控密码
-define("STATUS_PASSWORD","123456");
+define('IN_SYS', TRUE);
+if (!defined('IN_SYS')) {
+    　　exit('禁止访问');
+    } 
+//获取当前域名
+if ($config_customurl == true) {
+    define("TOOLS_URL",$config_ktools_custom_url);
+}else{
+    define("TOOLS_URL",$_SERVER['SERVER_NAME']);
+}
+// 状态监控
+define("STATUS_PASSWORD",$config_ktools_password);
